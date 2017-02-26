590fb84765264ff42d9516bb5facafdb2a2fae5c
Stefan Holzmüller <stefanholzmueller@github>
2017-02-26 19:33:15 +0100

    refactoring

--- index.js
+++ index.js
@@ -14,13 +14,26 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{
 
 
 var countryNames = countries.map(place => place.address.country);
-var askedCountryName;
+var state = {
+  target: randomElement(countryNames)
+}
+
+function render() {
+  Object.keys(state).forEach(function(key) {
+    const value = state[key];
+    document.getElementById(key).textContent = value;
+  })
+}
 
 function randomize() {
-  askedCountryName = countryNames[Math.floor(Math.random()*countryNames.length)];
-  document.getElementById('asked').textContent = askedCountryName;
+  state.target = randomElement(countryNames);
+  render();
 }
 randomize();
+
+function randomElement(array) {
+  return array[Math.floor(Math.random()*array.length)];
+}
 
 function buildUrl(baseUrl, queryParams) {
   return baseUrl + '?' + Object.keys(queryParams).map(key => key + '=' + queryParams[key]).join('&');
@@ -32,12 +45,16 @@ function getJson(baseUrl, queryParams, callback) {
 
 map.on('click', function(ev) {
   getJson('https://nominatim.openstreetmap.org/reverse', { format: 'json', lang: 'en', lat: ev.latlng.lat, lon: ev.latlng.lng, zoom: 3 }).then(function(json) {
-    var countryName = json.address.country;
-    if (countryName === askedCountryName) {
-      alert('You correctly located ' + countryName + '. Good job!');
-      randomize();
+    if (json.error) {
+      console.log('reverse geocoding error: ' + json.error);
     } else {
-      alert('You clicked on ' + (countryName || 'the sea') + ', not ' + askedCountryName + '. Try again!');
+      var countryName = json.address.country;
+      if (countryName === state.target) {
+        alert('You correctly located ' + countryName + '. Good job!');
+        randomize();
+      } else {
+        alert('You clicked on ' + (countryName || 'the sea') + ', not ' + state.target + '. Try again!');
+      }
     }
   })
 });
